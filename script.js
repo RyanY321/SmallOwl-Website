89dfdf741c65e47a9c75c31a664110ac838b8823
RyanY321 <86565861+RyanY321@users.noreply.github.com>
2022-06-21 12:58:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+alert('Welcome to SmallOwl Official.')
+alert('This page is currently being redone by Melior Media.')
+alert('Uh. Also... SmallOwl is a nerd.');
+
 document.getElementById('circle').addEventListener('click', function(){
     alert('SmallOwl is a nerd');
 });
