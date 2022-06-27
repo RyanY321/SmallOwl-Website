d56797adb7a4ac9d600233e9a20e441ac71e07db
RyanY321 <86565861+RyanY321@users.noreply.github.com>
2022-06-26 22:19:37 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-alert('Welcome to SmallOwl Official.')
-alert('This page is currently being redone by Melior Media.')
-alert('Uh. Also... SmallOwl is a nerd.');
-
-document.getElementById('circle').addEventListener('click', function(){
-    alert('SmallOwl is a nerd');
-});
-
-console.log('SmallOwl is a nerd');
